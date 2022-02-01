6d72cd512306c0262f795378e1bbd8b8b31161f1
Stanley Pan <imoment.developer@gmail.com>
2022-02-01 16:29:42 -0500

    Conditional color rendering based on dotState, complete rough animation

--- Dot_Inversion/Components/DotInversion.swift
+++ Dot_Inversion/Components/DotInversion.swift
@@ -12,16 +12,24 @@ struct DotInversion: View {
     // CurrentState
     @State var dotState: DotState = .normal
     @State var dotScale: CGFloat = 1
+    // Rotation
+    @State var dotRotation: Double = 0.0
     
     var body: some View {
         ZStack {
-            Color("brightOrange")
+            
+            ZStack {
+                // Changing color based on state
+                (dotState == .normal ? Color("brightOrange") : Color("darkGrey"))
+            }
+            .animation(.none, value: dotState)
             
             Rectangle()
-                .fill(Color("darkGrey"))
+                .fill(dotState != .normal ? Color("brightOrange") : Color("darkGrey"))
                 .overlay(
                     ExpandedView()
                 )
+                .animation(.none, value: dotState)
             // Masking view with circle to create dot inversion animation
                 .mask(
                     GeometryReader { proxy in
@@ -29,6 +37,7 @@ struct DotInversion: View {
                         // While increasing the scale the content will be visible
                             .frame(width: 100, height: 100)
                             .scaleEffect(dotScale)
+                            .rotation3DEffect(.init(degrees: dotRotation), axis: (x: 0, y: 1, z: 0), anchorZ: 10, perspective: 1)
                             .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
                             .offset(y: -60)
                     }
@@ -45,9 +54,16 @@ struct DotInversion: View {
                             dotState = .normal
                         }
                     } else {
+                        // At duration 0.75, reset scale to 1 to create inversion effect
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.725) {
+                            withAnimation(.linear(duration: 1.5)) {
+                                dotScale = 1
+                                dotState = .flipped
+                            }
+                        }
                         withAnimation(.linear(duration: 1.5)) {
+                            dotRotation = -180
                             dotScale = 8
-                            dotState = .flipped
                         }
                     }
                 })
