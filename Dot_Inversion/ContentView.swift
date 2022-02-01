84f5c5835712ac58b80a48eeef55956ac14aa36b
Stanley Pan <imoment.developer@gmail.com>
2022-02-01 15:31:19 -0500

    Initial Commit

---
+++ Dot_Inversion/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dot_Inversion
+//
+//  Created by Stanley Pan on 2022/02/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dot_Inversion/Dot_InversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dot_InversionApp.swift
+//  Dot_Inversion
+//
+//  Created by Stanley Pan on 2022/02/01.
+//
+
+import SwiftUI
+
+@main
+struct Dot_InversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
